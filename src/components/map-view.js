a3ce603e73f07178d9d2895684834d624f5aedf6
Dave Timmins <davetimmins@outlook.com>
2017-11-11 14:48:27 +1300

    update legend control dep

--- src/components/map-view.js
+++ src/components/map-view.js
@@ -12,14 +12,6 @@ class MapUi extends Component {
   };
   state = this.initialState;
   
-
-  getUrlParameter = name => {
-    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-    var results = regex.exec(location.search);
-    return results === null ? null : decodeURIComponent(results[1].replace(/\+/g, ' '));
-  };
-
   render() {
     const { mapId, initLegend, options } = this.props;
     const { title } = this.state;
@@ -30,70 +22,45 @@ class MapUi extends Component {
         modulesToLoad={[
           'esri/Map',
           isWebGLEnabled() && !isMobile() ? 'esri/views/SceneView' : 'esri/views/MapView',
-          'esri/WebMap',
           'esri/layers/MapImageLayer',
           'esri/layers/FeatureLayer',
         ]}    
-        onReady={({loadedModules: [Map, View, WebMap, MapImageLayer, FeatureLayer], containerNode}) => {
-          
-          const webMapId = this.getUrlParameter('webmap');
-          
-          if (webMapId) {
-            
-            const view = new View({
-              container: containerNode,
-              map: new WebMap({
-                portalItem: {
-                  id: webMapId
-                }
-              }),
-              padding: { right: 280 }
-            });
-  
-            view.map.portalItem.then(() => {
-              this.setState({ title: view.map.portalItem.title });
-            });
-  
-            // calling this initialises the legend control
-            initLegend(view, mapId);                
-          } 
-          else {
-            
-            const layer1 = new MapImageLayer({
-              url: 'https://sampleserver6.arcgisonline.com/arcgis/rest/services/RedlandsEmergencyVehicles/MapServer'
-            });
-  
-            const layer2 = new MapImageLayer({
-              url: 'https://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer',
-              visible: false
-            });
-  
-            const layer3 = new MapImageLayer({
-              url: 'https://sampleserver6.arcgisonline.com/arcgis/rest/services/Hurricanes/MapServer/'
-            });
+        onReady={({loadedModules: [Map, View, MapImageLayer, FeatureLayer], containerNode}) => {
+                      
+          const layer1 = new MapImageLayer({
+            url: 'https://sampleserver6.arcgisonline.com/arcgis/rest/services/RedlandsEmergencyVehicles/MapServer'
+          });
 
-            const layer4 = new FeatureLayer({
-              url: 'http://services2.arcgis.com/j80Jz20at6Bi0thr/arcgis/rest/services/HawaiiLavaFlowHazardZones/FeatureServer/0'
-            });
-  
-            const map = new Map({
-              basemap: 'topo',
-              layers: [layer1, layer4, layer2, layer3]
-            });
-  
-            const view = new View({
-              container: containerNode,
-              map: map,
-              padding: { right: 280 }
-            });
-  
-            // calling this initialises the legend control
-            initLegend(view, mapId);
-  
-            layer3.then(function(lyr) {
-              view.goTo(lyr.fullExtent);
-            });                
-          }
+          const layer2 = new MapImageLayer({
+            url: 'https://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer',
+            visible: false
+          });
+
+          const layer3 = new MapImageLayer({
+            url: 'https://sampleserver6.arcgisonline.com/arcgis/rest/services/Hurricanes/MapServer/'
+          });
+
+          const layer4 = new FeatureLayer({
+            url: 'http://services2.arcgis.com/j80Jz20at6Bi0thr/arcgis/rest/services/HawaiiLavaFlowHazardZones/FeatureServer/0'
+          });
+
+          const map = new Map({
+            basemap: 'topo',
+            layers: [layer1, layer4, layer2, layer3]
+          });
+
+          const view = new View({
+            container: containerNode,
+            map: map,
+            padding: { right: 280 }
+          });
+
+          // calling this initialises the legend control
+          initLegend(view, mapId);
+
+          layer3.then(function(lyr) {
+            view.goTo(lyr.fullExtent);
+          });    
         }}
         onError={error => console.error(error)}
       >
